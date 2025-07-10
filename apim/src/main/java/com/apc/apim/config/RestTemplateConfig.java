e3d903a7e1e49ec110c7652a1523ef0f457a7045
apc <apicoyc@gmail.com>
2025-07-10 11:32:44 -0500

    Comentarios explicativos

--- apim/src/main/java/com/apc/apim/config/RestTemplateConfig.java
+++ apim/src/main/java/com/apc/apim/config/RestTemplateConfig.java
@@ -7,6 +7,13 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+    /*
+     * Con @LoadBalanced, indicamos a Spring Boot que cada vez que RestTemplate
+     * realice una petición a una URL con un hostname que no sea real, lo consulte
+     * con Eureka. Por ejemplo: http://apimatricula/api/v1/cursos.
+     * En cada llamanda, Ribbon consulta a Eureka por las instancias y escoge una,
+     * utilizando balanceo de carga (por defecto round-robin).
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
